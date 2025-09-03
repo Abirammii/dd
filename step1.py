92db62a53b9373cf7503a20f280fd19c5f285887
Abirami K <abirammii.k@gmail.com>
2025-09-03 16:21:31 +0530

    Create step1.py

---
+++ step1.py
@@ -0,0 +1,14 @@
+# Step 1: Install required libraries
+!pip install pandas numpy plotly scikit-learn seaborn streamlit pyngrok openpyxl
+
+# Import libraries
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+print("✅ Environment ready!")
